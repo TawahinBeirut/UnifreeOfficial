e2a318d09732394f624d31481d9c76ea62d0e47b
Gatta <gatta.code@gmail.com>
2024-03-28 14:32:48 +0100

    changed style bug

--- src/components/UniGpt/Chat.jsx
+++ src/components/UniGpt/Chat.jsx
@@ -1,7 +1,7 @@
 export default function Chat({ User, Text, mode }) {
   console.log(User)
     return (
-      <div className={`p-5 drop-shadow-xl rounded-xl bg-white ${User == "UniGPT" ? " bg-[#D881FF] font-bold text-white mr-8" : "text-right ml-auto"}`}>
+      <div className={`p-5 drop-shadow-xl rounded-xl bg-white ${User == "UniGPT" ? " bg-purple-400 font-bold text-white mr-8" : "text-right ml-auto"}`}>
         <p className="font-bold">{User}</p>
         <p>{Text}</p>
       </div>
